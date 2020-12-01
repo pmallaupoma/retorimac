34e4bb6eb9a98a4ed9f09bb6901cc880ebacf415
Pedro <pedromallaupoma@gmail.com>
2020-11-30 21:52:45 -0500

    Version con translate de keys

--- swapi/people/listpeople.js
+++ swapi/people/listpeople.js
@@ -15,13 +15,45 @@ module.exports.get = (event, context, callback) => {
 
     const req = https.request(options, (res) => {
         let body = '';
+
         res.setEncoding('utf-8');
+
         res.on('data', (chunk) => body += chunk);
+
         res.on('end', () => {
 
             console.log('Responses HTTPS procesado exitosamente');
-            body = JSON.parse(body);
 
+            //AGREGADO PARA TRANSLATE
+            body = JSON.parse(body);
+            // Funcion para Renombrar Key de JSON
+            function renameKey() {
+
+                body = JSON.parse(JSON.stringify(body).split('"count":').join(process.env.Contar));
+                body = JSON.parse(JSON.stringify(body).split('"next":').join(process.env.Siguiente));
+                body = JSON.parse(JSON.stringify(body).split('"previous":').join(process.env.Anterior));
+                body = JSON.parse(JSON.stringify(body).split('"results":').join(process.env.Resultados));
+                body = JSON.parse(JSON.stringify(body).split('"name":').join(process.env.Nombre));
+                body = JSON.parse(JSON.stringify(body).split('"height":').join(process.env.Altura));
+                body = JSON.parse(JSON.stringify(body).split('"mass":').join(process.env.Masa));
+                body = JSON.parse(JSON.stringify(body).split('"hair_color":').join(process.env.Color_Pelo));
+                body = JSON.parse(JSON.stringify(body).split('"skin_color":').join(process.env.Color_Piel));
+                body = JSON.parse(JSON.stringify(body).split('"eye_color":').join(process.env.Color_Ojos));
+                body = JSON.parse(JSON.stringify(body).split('"birth_year":').join(process.env.Ano_Nacimiento));
+                body = JSON.parse(JSON.stringify(body).split('"gender":').join(process.env.Genero));
+                body = JSON.parse(JSON.stringify(body).split('"homeworld":').join(process.env.Mundo_Natal));
+                body = JSON.parse(JSON.stringify(body).split('"films":').join(process.env.Peliculas));
+                body = JSON.parse(JSON.stringify(body).split('"species":').join(process.env.Especies));
+                body = JSON.parse(JSON.stringify(body).split('"vehicles":').join(process.env.Vehiculos));
+                body = JSON.parse(JSON.stringify(body).split('"starships":').join(process.env.Naves_Estelares));
+                body = JSON.parse(JSON.stringify(body).split('"created":').join(process.env.Creado));
+                body = JSON.parse(JSON.stringify(body).split('"edited":').join(process.env.Editado));
+
+            }
+            renameKey();
+
+
+            /////////
             callback(null, {
                 statusCode: 200,
                 body: JSON.stringify(body),
